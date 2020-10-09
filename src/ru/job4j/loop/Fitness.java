6481559908939e4be687a2529a27dcccb0b13e82
Eduard Bucari <eduard.bucari@gmail.com>
2020-10-10 01:38:18 +0300

    5.4. Протеиновая диета [#360918] (2)

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,12 +3,12 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-        while (month < 5) {
 
-            ivan = ivan * 3;
+        while (ivan < nik) {
+            ivan *= 3;
+            nik *= 2;
+            month++;
         }
         return month;
     }
 }
-// ivan = ivan * 3;
-// nic = nic * 2;
