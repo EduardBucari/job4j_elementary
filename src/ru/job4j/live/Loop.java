ec2e1bd95347fe3a5b9f20dc5e86a81825bc0ee4
Eduard Bucari <eduard.bucari@gmail.com>
2020-10-14 01:16:03 +0300

    5.9.1. Генерация конструкций в IDEA [#360925]

---
+++ src/ru/job4j/live/Loop.java
@@ -0,0 +1,11 @@
+package ru.job4j.live;
+
+public class Loop {
+    public static void main(String[] args) { // psvm + tab
+        for (int i = 0; i < 10; i++) {       // fori + tab
+            System.out.println("Thanks");    // sout + tab
+        }
+    }
+
+
+}
