207fcc127315f0bfbc4adcaf74abeb78a5ad00d4
Eduard Bucari <eduard.bucari@gmail.com>
2020-10-15 16:30:06 +0300

    5.9. Крест в псевдографике. [#360922](4)

--- src/ru/job4j/loop/Slash.java
+++ src/ru/job4j/loop/Slash.java
@@ -2,11 +2,11 @@ package ru.job4j.loop;
 
 public class Slash {
     public static void draw(int size) {
-        for (int row = 0; row < size; row++) {
-            for (int cell = 0; cell < size; cell++) {
+        for (int row = 0; row <= size; row++) {
+            for (int cell = 0; cell <= size; cell++) {
                 boolean left = row == cell;
 
-                boolean right = (row + cell + size) % 2 == 1;
+                boolean right = row + cell == size;
                 if (left) {
                     System.out.print("0");
                 } else if (right) {
