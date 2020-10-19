a60c9e3dd950664a245c4790d7910be15cc6f429
Eduard Bucari <eduard.bucari@gmail.com>
2020-10-20 00:53:47 +0300

    6.4. Слово начинается с ... [#360832](3)

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -3,10 +3,11 @@ package ru.job4j.array;
 public class ArrayChar {
 
     public static boolean startWith(char[] word, char[] pref) {
-        boolean result = false;
+        boolean result = true;
         for (int i = 0; i < word.length; i++) {
                 if (pref[i] != word[i]) {
-                    break;
+                    result = false;
+                  break;
                 }
 
         }
