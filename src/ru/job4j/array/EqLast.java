04a4da11ef06b7e247a7eee2b8cc9353d627d9d1
Eduard Bucari <eduard.bucari@gmail.com>
2020-10-22 01:17:20 +0300

    6.4.1. Сравнить последние элементы двух массивов [#360851](3)

--- src/ru/job4j/array/EqLast.java
+++ src/ru/job4j/array/EqLast.java
@@ -4,10 +4,7 @@ public class EqLast {
     public static boolean check(int[] left, int[] right) {
        int lastNum1 = left[left.length - 1];
        int lastNum2 = right[right.length - 1];
-        if (lastNum1 == lastNum2) {
-            return true;
-        } else {
-            return false;
+            return lastNum1 == lastNum2;
         }
     }
-}
+
--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -11,4 +11,12 @@ public class FindLoop {
         }
         return  rst;
     }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1; /* если элемента нет в массиве, то возвращаем -1. */
+        for (int j = 0; j < data.length; j++) {
+           // if (data[j] == )
+        }
+        return rst;
+    }
 }
