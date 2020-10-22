ac2979d2047099491f3bbee86d63fdeda2f76dc5
Eduard Bucari <eduard.bucari@gmail.com>
2020-10-23 02:12:24 +0300

    6.5.3. Сортировка выборкой. [#360838]

---
+++ src/ru/job4j/array/SortSelected.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class SortSelected {
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, 0, data.length);
+            int index = FindLoop.indexOf(data, min, 0, data.length);
+            // swap()
+            int temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+        return data;
+    }
+}
+
