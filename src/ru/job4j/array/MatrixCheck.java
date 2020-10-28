4f86fde45c554981bc975a597dc67a48a0507ba1
Eduard Bucari <eduard.bucari@gmail.com>
2020-10-29 00:51:38 +0300

    6.7.4. Выигрышные комбинации в сокобан [#360834](3)

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -36,6 +36,9 @@ public class MatrixCheck {
         for (int index = 0; index < board.length; index++) {
             if (board[index][index] == 'x') {
                 result = true;
+                if (monoHorizontal(board, index) || monoVertical(board, index)) {
+                    result = true;
+                }
                 break;
             }
         }
---
+++ src/ru/job4j/array/Zadachi6.java
@@ -0,0 +1,7 @@
+package ru.job4j.array;
+
+//пройтись "улиткой", от левого верхнего угла до центра.
+public class Zadachi6 {
+
+}
+//45.28
