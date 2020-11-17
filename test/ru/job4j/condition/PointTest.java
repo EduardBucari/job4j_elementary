2cd4f96cf9399b5dbfd955f26a9e1a7d021f6fdb
Eduard Bucari <eduard.bucari@gmail.com>
2020-11-17 21:57:29 +0300

    2.1. Рефакторинг - Расстояние между точками. Test

--- test/ru/job4j/condition/PointTest.java
+++ test/ru/job4j/condition/PointTest.java
@@ -1,44 +1,58 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
+//import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class PointTest {
+//public class PointTest {
 
-    @Test
-    public void when00to20then2() {
-        int expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected, out, 0.01);
-    }
+   //@Test
+   // public void when00to20then2() {
+    //    int expected = 2;
+       // int x1 = 0;
+       // int y1 = 0;
+       // int x2 = 2;
+       // int y2 = 0;
 
-    @Test
-    public void when33to93then6() {
-        int expected2 = 6;
-        int x1 = 3;
-        int y1 = 3;
-        int x2 = 9;
-        int y2 = 3;
-        double out2 = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected2, out2, 0.01);
-    }
 
+     //   double out = Point.distance(x1, y1, x2, y2);
+
+   //     Assert.assertEquals(expected, out, 0.01);
+  //  }
+
+ //   @Test
+  //  public void when33to93then6() {
+  //      int expected2 = 6;
+  //      int x1 = 3;
+   //     int y1 = 3;
+   //     int x2 = 9;
+   //     int y2 = 3;
+   //     double out2 = Point.distance(x1, y1, x2, y2);
+   //     Assert.assertEquals(expected2, out2, 0.01);
+ //   }
+
+  //  @Test
+ //   public void when11to41then3() {
+ //       int expected3 = 3;
+ //       int x1 = 1;
+ //       int y1 = 1;
+  //      int x2 = 4;
+  //      int y2 = 1;
+  //      double out3 = Point.distance(x1, y1, x2, y2);
+ //       Assert.assertEquals(expected3, out3, 0.01);
+ //   }
+//}
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+  public class PointTest {
     @Test
-    public void when11to41then3() {
-        int expected3 = 3;
-        int x1 = 1;
-        int y1 = 1;
-        int x2 = 4;
-        int y2 = 1;
-        double out3 = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(expected3, out3, 0.01);
+      public void distance() {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        assertThat(dist, closeTo(2, 0.01));
     }
 }
-// спасибо за задание,
-// надеюсь я все правильно понял.
